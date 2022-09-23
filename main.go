df25e9ea8accf79c94a1b448b0b8c83a7c0bb5c8
Rodrigo <rodrigosangali@gmail.com>
2022-09-23 16:02:45 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/htpp"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	r:= gin.Default()
+	r.GET("/ping", func(c *.gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong"
+		})
+	})
+	Connect()
+	
+	r.Run()
+}
